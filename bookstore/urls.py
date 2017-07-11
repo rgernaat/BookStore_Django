0e110bf04815740d4517fe08b7fa5a0d7f807455
rgernaat <rgernaat@users.noreply.github.com>
2017-07-11 14:11:30 -0700

    Add files via upload

---
+++ bookstore/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'bookstore.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^store/', include('store.urls'), name='store'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url('', include('social.apps.django_app.urls', namespace = "social")),
+    url(r'^admin/', include(admin.site.urls)),
+]
